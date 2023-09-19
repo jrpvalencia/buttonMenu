0626d0236c0d18b92edb53acc524d9ccbf1ac52b
jose robinson puliche valencia <jrpvalencia612@gmail.com>
2023-09-19 18:59:05 -0500

    trabajo funcional

---
+++ app/src/main/java/com/example/buttonnavigation/MainActivity.kt
@@ -0,0 +1,39 @@
+
+package com.example.buttonnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarMenuView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val firstFragment=firstFragment()
+        val secondFragment=secondFragment()
+        val thirdFragment=thirdFragment()
+
+        setCurrentFragment(firstFragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        @Suppress("DEPRECATION")
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(firstFragment)
+                R.id.person->setCurrentFragment(secondFragment)
+                R.id.settings->setCurrentFragment(thirdFragment)
+
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+}
